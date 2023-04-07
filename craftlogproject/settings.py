ad186f2ffc91a8bdecc1936cdf7410e0575cae6f
ubuntu <ubuntu@os3-299-39470.vs.sakura.ne.jp>
2023-04-07 10:21:54 +0900

    230407_production

--- craftlogproject/settings.py
+++ craftlogproject/settings.py
@@ -21,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG') == 'True'
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+DEBUG = env('DEBUG') 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+#ALLOWED_HOSTS = ['49.212.179.224']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -82,13 +83,16 @@ WSGI_APPLICATION = 'craftlogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+DATABASES = {
+        'default' : env.db(),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -125,8 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'medi/'
 MEDIA_ROOT = BASE_DIR / 'media'
